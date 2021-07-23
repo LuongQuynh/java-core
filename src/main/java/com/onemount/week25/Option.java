27c220b86049dc09c2cc4b439268c7abcbf4efdc
LuongQuynh <quynhlt2511@gmail.com>
2021-07-23 23:38:42 +0700

    delete folder

--- src/main/java/com/onemount/week25/Option.java
+++
@@ -1,6 +0,0 @@
-package com.onemount.week25;
-
-public enum Option {
-    BLUE, PINK, WHITE, BLACK
-}
-
--- src/main/java/com/onemount/week25/PrimitiveDataTypes.java
+++
@@ -1,9 +0,0 @@
-package com.onemount.week25;
-
-public class PrimitiveDataTypes {
-    public static void main(String[] args) {
-        int i = 6;
-        Integer a;
-        Boolean b;
-    }
-}
